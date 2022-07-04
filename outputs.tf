bd5af22f885ca2c263047d702e27b7f5d29d8344
cednore <cednore@norumin.com>
2022-07-04 06:44:41 -0700

    ✨ output "app_image_build_args"

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,26 @@ output "app_lb_dns_name" {
   value       = module.end.app_lb_dns_name
   sensitive   = true
 }
+
+output "app_image_build_args" {
+  description = "Build-time arguments of app docker image"
+  value = {
+    NEXT_PUBLIC_WEBAPP_URL      = "https://${var.domain}"
+    NEXT_PUBLIC_APP_URL         = "https://${var.domain}"
+    NEXT_PUBLIC_LICENSE_CONSENT = "agree"
+    CALCOM_TELEMETRY_DISABLED   = "1"
+    DATABASE_URL = join("", [
+      "postgresql://",
+      data.aws_secretsmanager_secret_version.app_env.db_username,
+      ":",
+      data.aws_secretsmanager_secret_version.app_env.db_password,
+      "@",
+      module.data.db_endpoint,
+      "/",
+      data.aws_secretsmanager_secret_version.app_env.db_name
+    ])
+    NEXTAUTH_SECRET         = data.aws_secretsmanager_secret_version.auth_secret
+    CALENDSO_ENCRYPTION_KEY = data.aws_secretsmanager_secret_version.encryption_key
+  }
+  sensitive = true
+}
