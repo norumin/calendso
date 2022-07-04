1e0eceda30cdedb12481133cd96c51484a41bb03
cednore <cednore@norumin.com>
2022-07-03 19:40:23 -0700

    🐛 fix locals

--- modules/calendso-build/docker.tf
+++ modules/calendso-build/docker.tf
@@ -14,13 +14,13 @@ resource "null_resource" "app_image_builder" {
   provisioner "local-exec" {
     command = <<BASH
                 docker build ${var.src_repo}#${var.src_revision} \
-                  --build-arg NEXT_PUBLIC_WEBAPP_URL=${build_args.NEXT_PUBLIC_WEBAPP_URL} \
-                  --build-arg NEXT_PUBLIC_APP_URL=${build_args.NEXT_PUBLIC_APP_URL} \
-                  --build-arg NEXT_PUBLIC_LICENSE_CONSENT=${build_args.NEXT_PUBLIC_LICENSE_CONSENT} \
-                  --build-arg CALCOM_TELEMETRY_DISABLED=${build_args.CALCOM_TELEMETRY_DISABLED} \
-                  --build-arg DATABASE_URL=${build_args.DATABASE_URL} \
-                  --build-arg NEXTAUTH_SECRET=${build_args.NEXTAUTH_SECRET} \
-                  --build-arg CALENDSO_ENCRYPTION_KEY=${build_args.CALENDSO_ENCRYPTION_KEY} \
+                  --build-arg NEXT_PUBLIC_WEBAPP_URL=${local.build_args.NEXT_PUBLIC_WEBAPP_URL} \
+                  --build-arg NEXT_PUBLIC_APP_URL=${local.build_args.NEXT_PUBLIC_APP_URL} \
+                  --build-arg NEXT_PUBLIC_LICENSE_CONSENT=${local.build_args.NEXT_PUBLIC_LICENSE_CONSENT} \
+                  --build-arg CALCOM_TELEMETRY_DISABLED=${local.build_args.CALCOM_TELEMETRY_DISABLED} \
+                  --build-arg DATABASE_URL=${local.build_args.DATABASE_URL} \
+                  --build-arg NEXTAUTH_SECRET=${local.build_args.NEXTAUTH_SECRET} \
+                  --build-arg CALENDSO_ENCRYPTION_KEY=${local.build_args.CALENDSO_ENCRYPTION_KEY} \
                   -t ${var.image_registry}/${var.image_name}:latest \
                   -t ${var.image_registry}/${var.image_name}:${var.image_tag}
               BASH
