5ffab971571c767b411458e0082931c9e43dde10
cednore <cednore@norumin.com>
2022-07-18 14:48:47 -0700

    ⚡ better ni def

--- modules/calendso-app/ec2.tf
+++ modules/calendso-app/ec2.tf
@@ -1,13 +1,31 @@
 resource "aws_instance" "app" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  key_name               = aws_key_pair.main.key_name
-  vpc_security_group_ids = var.sg_ids
-  iam_instance_profile   = aws_iam_instance_profile.app.id
+  ami                  = data.aws_ami.ubuntu.id
+  instance_type        = var.instance_type
+  key_name             = aws_key_pair.main.key_name
+  iam_instance_profile = aws_iam_instance_profile.app.id
+
+  network_interface {
+    network_interface_id = aws_network_interface.app.id
+    device_index         = 0
+  }
 
   tags = merge(local.default_tags, {
     Name = "i-${local.slug}-app"
+  })
+
+  lifecycle {
+    ignore_changes = [
+      ami, # Ignore ami updates
+    ]
+  }
+}
+
+resource "aws_network_interface" "app" {
+  subnet_id       = var.subnet_id
+  security_groups = var.sg_ids
+
+  tags = merge(local.default_tags, {
+    Name = "ni-${local.slug}-app"
   })
 }
 
